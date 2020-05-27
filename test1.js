db1815cfcdc6b99d14930681030a50965847e49d
pengtao <pengtao_it@163.com>
2020-05-27 23:52:13 +0800

    test

--- test1.js
+++ test1.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: PT
+ * @Date: 2020-05-27 23:49:33
+ * @LastEditors: PT
+ * @LastEditTime: 2020-05-27 23:52:03
+ * @Description: file content
+ */ 
 function test(){
 	console.log(111);
 }
@@ -5,3 +12,4 @@ function test(){
 function test2(){
 	console.log(222);
 }
+console.log('111')
